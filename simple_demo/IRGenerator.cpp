d54c2ad1d1dd5b54795c67955ce5f67337064ae3
zjuAIHz <2282753968@qq.com>
2023-05-24 00:55:21 +0800

    successful demo, we are best!

--- simple_demo/IRGenerator.cpp
+++ simple_demo/IRGenerator.cpp
@@ -29,32 +29,32 @@ void IRGenerator::GenObjectCode(std::string outputfile) {
 
     this->Module->print(llvm::outs(), NULL);
 
-    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
-    llvm::InitializeAllTargetInfos();
-    llvm::InitializeAllTargets();
-    llvm::InitializeAllTargetMCs();
-    llvm::InitializeAllAsmParsers();
-    llvm::InitializeAllAsmPrinters();
+    // auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+    // llvm::InitializeAllTargetInfos();
+    // llvm::InitializeAllTargets();
+    // llvm::InitializeAllTargetMCs();
+    // llvm::InitializeAllAsmParsers();
+    // llvm::InitializeAllAsmPrinters();
 
-    auto Features = "";
-    auto CPU = "generic";
-    std::string Error;
-    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+    // auto Features = "";
+    // auto CPU = "generic";
+    // std::string Error;
+    // auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
 
-    llvm::TargetOptions opt;
-    auto RM = llvm::Optional<llvm::Reloc::Model>();
-    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
-    Module->setDataLayout(TargetMachine->createDataLayout());
-    Module->setTargetTriple(TargetTriple);
+    // llvm::TargetOptions opt;
+    // auto RM = llvm::Optional<llvm::Reloc::Model>();
+    // auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+    // Module->setDataLayout(TargetMachine->createDataLayout());
+    // Module->setTargetTriple(TargetTriple);
 
-    std::error_code EC;
-    llvm::raw_fd_ostream Dest(outputfile, EC, llvm::sys::fs::OF_None);
+    // std::error_code EC;
+    // llvm::raw_fd_ostream Dest(outputfile, EC, llvm::sys::fs::OF_None);
 
-    auto FileType = llvm::CGFT_ObjectFile;
-    llvm::legacy::PassManager PM;
-    TargetMachine->addPassesToEmitFile(PM, Dest, nullptr, FileType);
+    // auto FileType = llvm::CGFT_ObjectFile;
+    // llvm::legacy::PassManager PM;
+    // TargetMachine->addPassesToEmitFile(PM, Dest, nullptr, FileType);
 
-    PM.run(*Module);
-    Dest.flush();
+    // PM.run(*Module);
+    // Dest.flush();
 
 }
--- simple_demo/IRGenerator.hpp
+++ simple_demo/IRGenerator.hpp
@@ -18,19 +18,20 @@
 #include <string>
 #include <exception>
 #include <llvm/IR/Value.h>
-#include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/PassManager.h>
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/CallingConv.h>
 #include <llvm/IR/IRPrintingPasses.h>
-#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/GlobalVariable.h>
-#include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/PassManager.h>
 #include <llvm/IR/ValueSymbolTable.h>
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IRReader/IRReader.h>
 #include <llvm/ExecutionEngine/MCJIT.h>
 #include <llvm/ExecutionEngine/Interpreter.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
@@ -41,18 +42,17 @@
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Host.h>
 #include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Passes/PassPlugin.h>
 #include <llvm/Transforms/InstCombine/InstCombine.h>
 #include <llvm/Transforms/Scalar.h>
 #include <llvm/Transforms/Scalar/GVN.h>
 #include <llvm/MC/TargetRegistry.h>
-#include <llvm/Support/FileSystem.h>
-#include <llvm/Support/Host.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Target/TargetMachine.h>
-#include <llvm/Target/TargetOptions.h>
 
 
 #include "AST.hpp"
