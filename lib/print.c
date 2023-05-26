6592bcb54c5db80517a8c49268b0eea43eded9b1
zjuAIHz <2282753968@qq.com>
2023-05-27 03:08:14 +0800

    successful dynamic lib

--- lib/print.c
+++ lib/print.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void printk(int addr, ...){
+void printk(int addr, int a){
     va_list valist; 
-    printf((char*)addr, valist);
+    printf((char*)addr, a);
 }
--- src/test.c
+++ src/test.c
@@ -2,7 +2,7 @@ int printk(int ptr, ...);
 
 int main(void){
  int a = 2;
- printk("%d\n", &a);
+ printk("%d\n", a);
  return 0;
 }
 
