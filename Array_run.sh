c861b6f71bad093b3d5e94f0eec93e0fcb676837
eagle-c7 <wenyu088807@163.com>
2023-05-28 04:47:08 +0800

    Pass Matrix test

---
+++ Array_run.sh
@@ -0,0 +1,3 @@
+./src/build/src ./tests/Array.c
+gcc -no-pie simpledemo.o lib/io.so
+./a.out
---
+++ tests/Array.c
@@ -0,0 +1,60 @@
+int printk(int ptr, ...);
+int scank(int ptr, ...);
+
+int main(){
+	int a2;
+	int b1;
+	int a1;
+	int b2;
+
+	int Arr1[1000];
+	int Arr2[1000];
+	int Res[1000];
+	int i;
+	int j;
+	int k;
+	int temp1;
+
+
+	scank("%d %d", &a1, &b1);
+
+	for(i = 0;i<a1;i=i+1){
+		for(j=0;j<b1;j=j+1){
+			scank("%d", &temp1);
+			Arr1[i*25+j] = temp1;
+		}
+	}
+
+	scank("%d %d ", &a2, &b2);
+	for(i = 0;i<a2;i=i+1){
+		for(j=0;j<b2;j=j+1){
+			scank("%d", &temp1);
+			Arr2[i*25+j] = temp1;
+		}
+	}
+	
+	if(b1 != a2){
+		printk("Incompatible Dimensions\n");
+		return 0;
+	}
+	for(i = 0;i<a1;i=i+1){
+		for(j=0;j<b2;j=j+1){
+			int sum = 0;
+			for(k=0;k<b1;k=k+1){
+				int temp1 = Arr1[i*25+k];
+				int temp2 = Arr2[k*25+j];
+				sum = sum + temp1 * temp2;
+			}
+			Res[i*25+j] = sum;
+		}
+	}
+	for(i=0;i<a1;i=i+1){
+		for(j=0;j<b2;j=j+1){
+			int temp = Res[i*25+j];
+			printk("%10d",temp);
+		}
+		printk("\n");
+	}
+
+	return 0;
+}
