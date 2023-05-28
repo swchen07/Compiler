6fa5c08735fb7679a1975d4d436e1794b2ee6242
zjuAIHz <2282753968@qq.com>
2023-05-28 16:09:51 +0800

    add advisor

---
+++ Advisor_run.sh
@@ -0,0 +1,3 @@
+./src/build/src ./tests/Advisor/Advisor.c
+gcc -no-pie Compiler.o lib/io.so -o tests/Advisor/Advisor.out
+../tester/advisor-linux-amd64 ./tests/Advisor/Advisor.out
--- Course_run.sh
+++
@@ -1,3 +0,0 @@
-./src/build/src ./tests/CourseSelector/courseSelector.c
-gcc -no-pie Compiler.o lib/io.so -o tests/CourseSelector/courseSelector.out
-../tester/quicksort-linux-amd64 ./tests/CourseSelector/courseSelector.out
