4937f633159705dd7e045621529847bd31c581bc
zjuAIHz <100405497+zjuAIHz@users.noreply.github.com>
2023-05-28 15:20:07 +0800

    Delete Sort_run.sh

--- Sort_run.sh
+++
@@ -1,3 +0,0 @@
-./src/build/src ./tests/QuickSort/quicksort.c
-gcc -no-pie Compiler.o lib/io.so -o tests/QuickSort/quickSort.out
-./Ref/tester/quicksort/quicksort-linux-amd64 ./tests/QuickSort/quickSort.out
